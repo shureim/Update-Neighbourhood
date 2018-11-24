d8d80259880c9b88c03f1a20bc71e688a01bbcca
shureim <tarickaliabdi@gmail.com>
2018-11-25 02:54:36 +0300

    created the neighborhood class in models.py

--- update/models.py
+++ update/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-#
-# # Create your models here.
-# class Neighborhood(models.Model):
-#     name = models.CharField(max_length = 50)
-#     location = models.CharField(max_length = 500)
-#     count = models.PositiveIntegerField()
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 500)
+    count = models.PositiveIntegerField()
 #
 #     def __str__(self):
 #         return self.name
