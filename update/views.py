6f8421d25034954eea8f00c4f7f9524e0449af76
shureim <tarickaliabdi@gmail.com>
2018-11-25 02:47:27 +0300

    created the edit_profile function in views.py

--- update/views.py
+++ update/views.py
@@ -71,19 +71,19 @@ def change_profile(request,user):
         form = ProfileForm()
     return render(request,'change_profile.html',{'form':form})
 
-# def edit_profile(request):
-#     current_user = request.user
-#     if request.method == 'POST':
-#         form = ProfileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             profile = form.save(commit = False)
-#             profile.user = current_user
-#             profile.save()
-#         return redirect('Profile')
-#     else:
-#         form = ProfileForm()
-#     return render(request,'edit_profile.html',{'form':form})
-#
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.user = current_user
+            profile.save()
+        return redirect('Profile')
+    else:
+        form = ProfileForm()
+    return render(request,'edit_profile.html',{'form':form})
+
 # def post(request,post_id):
 #     try:
 #         post = Post.objects.get(id = post_id)
